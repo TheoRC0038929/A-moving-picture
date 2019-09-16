92a691c1886d8dc4b0ef41bcbe2efa2b9516903f
Theo Jack Roberts <rc0038929@student.riversidecollege.ac.uk>
2019-09-16 15:06:19 +0100

    hello

--- A moving picture/Form1.cs
+++ A moving picture/Form1.cs
@@ -24,12 +24,18 @@ namespace A_moving_picture
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X + 10, pictureBox1.Location.Y);
+            if (pictureBox1.Location.X +5 < ClientRectangle.Width)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 10, pictureBox1.Location.Y);
+            }
         }
 
         private void Button3_Click(object sender, EventArgs e)
-        {
-            pictureBox1.Location = new Point(pictureBox1.Location.X - 10, pictureBox1.Location.Y);
+        {  
+            if (pictureBox1.Location.X > 0)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 10, pictureBox1.Location.Y);
+            }
         }
 
         private void Button4_Click(object sender, EventArgs e)
